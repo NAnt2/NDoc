7dbd55af2aa192bed297b495d0c8a989ec6b4cf0
cvs2git <none@users.sourceforge.net>
2001-11-12 05:59:19 +0000

    This commit was manufactured by cvs2git to create branch 'VS_Beta_2'.

--- ndoc/web/deploy.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-if %1x == x goto usage
-scp style.css index.html screenshots.html cvs.html %1@ndoc.sourceforge.net:/home/groups/n/nd/ndoc/htdocs
-goto end
-:usage
-echo usage: deploy username
-:end
